2495dfcd3bba35a10f70edab21cabe1d353553d5
tianya <tianya@imcollege.cn>
2020-08-10 16:02:39 +0800

    修正后台XSS存在的安全问题

--- src/dede/action_search.php
+++ src/dede/action_search.php
@@ -14,7 +14,7 @@ require_once(dirname(__FILE__)."/actionsearch_class.php");
 //增加权限检查
 if(empty($dopost)) $dopost = "";
 
-$keyword=empty($keyword)? "" : $keyword;
+$keyword=empty($keyword)? "" : RemoveXss($keyword);
 $actsearch = new ActionSearch($keyword);
 $asresult = $actsearch->Search();
 include DedeInclude('templets/action_search.htm');
