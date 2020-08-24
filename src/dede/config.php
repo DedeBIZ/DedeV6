de68bca7d1b5ef2b213a67fa24056f35b7585629
tianya <tianya@imcollege.cn>
2020-08-24 11:57:45 +0800

    修复CSRF保护措施绕过漏洞

--- src/dede/config.php
+++ src/dede/config.php
@@ -64,7 +64,7 @@ function csrf_check()
 {
     global $token;
 
-    if(!isset($token) || strcasecmp($token, $_SESSION['token']) != 0){
+    if(!isset($token) || strcasecmp($token, $_SESSION['token']) !== 0){
         echo '<a href="http://bbs.dedecms.com/907721.html">DedeCMS:CSRF Token Check Failed!</a>';
         exit;
     }
