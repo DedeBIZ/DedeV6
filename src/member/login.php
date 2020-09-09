4609d9c5146c8aa683143a207cf39282fa824427
tianya <tianya@imcollege.cn>
2020-09-09 19:45:59 +0800

    移除默认模板中无效资源

--- src/member/login.php
+++ src/member/login.php
@@ -10,7 +10,7 @@ require_once(dirname(__FILE__)."/config.php");
 $gourl = RemoveXSS($gourl);
 if($cfg_ml->IsLogin())
 {
-    ShowMsg('你已经登陆系统，无需重新注册！', 'index.php');
+    ShowMsg('你已经登录系统，无需重新注册！', 'index.php');
     exit();
 }
 require_once(dirname(__FILE__)."/templets/login.htm");
