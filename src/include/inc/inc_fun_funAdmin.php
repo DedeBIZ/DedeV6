50344b22705157b8bcf84979a89e13378384bc57
tianya <tianya@imcollege.cn>
2020-11-17 07:19:00 +0800

    编辑器增加插入mp4视频链接

--- src/include/inc/inc_fun_funAdmin.php
+++ src/include/inc/inc_fun_funAdmin.php
@@ -1,4 +1,4 @@
-<?php   if(!defined('DEDEINC')) exit('dedecms');
+<?php if (!defined('DEDEINC')) exit('dedecms');
 /**
  * 管理员后台基本函数
  *
@@ -18,10 +18,10 @@
  * @param     int  $isclose  解析后是否释放资源
  * @return    string
  */
-function SpGetPinyin($str, $ishead=0, $isclose=1)
+function SpGetPinyin($str, $ishead = 0, $isclose = 1)
 {
     global $pinyins;
-    global $cfg_bizcore_appid,$cfg_bizcore_key,$cfg_bizcore_hostname,$cfg_bizcore_port;
+    global $cfg_bizcore_appid, $cfg_bizcore_key, $cfg_bizcore_hostname, $cfg_bizcore_port;
     global $cfg_soft_lang;
     $restr = '';
     if (!empty($cfg_bizcore_appid) && !empty($cfg_bizcore_key)) {
@@ -31,57 +31,43 @@ function SpGetPinyin($str, $ishead=0, $isclose=1)
         $client = new DedeBizClient($cfg_bizcore_hostname, $cfg_bizcore_port);
         $client->appid = $cfg_bizcore_appid;
         $client->key = $cfg_bizcore_key;
-        $data = $client->Pinyin($str,"");
+        $data = $client->Pinyin($str, "");
         $restr = $data->data;
         $client->Close();
     } else {
         $str = trim($str);
         $slen = strlen($str);
-        if($slen < 2)
-        {
+        if ($slen < 2) {
             return $str;
         }
-        if(@count($pinyins) == 0)
-        {
-            $fp = fopen(DEDEINC.'/data/pinyin.dat', 'r');
-            while(!feof($fp))
-            {
+        if (@count($pinyins) == 0) {
+            $fp = fopen(DEDEINC . '/data/pinyin.dat', 'r');
+            while (!feof($fp)) {
                 $line = trim(fgets($fp));
-                $pinyins[$line[0].$line[1]] = substr($line, 3, strlen($line)-3);
+                $pinyins[$line[0] . $line[1]] = substr($line, 3, strlen($line) - 3);
             }
             fclose($fp);
         }
-        for($i=0; $i<$slen; $i++)
-        {
-            if(ord($str[$i])>0x80)
-            {
-                $c = $str[$i].$str[$i+1];
+        for ($i = 0; $i < $slen; $i++) {
+            if (ord($str[$i]) > 0x80) {
+                $c = $str[$i] . $str[$i + 1];
                 $i++;
-                if(isset($pinyins[$c]))
-                {
-                    if($ishead==0)
-                    {
+                if (isset($pinyins[$c])) {
+                    if ($ishead == 0) {
                         $restr .= $pinyins[$c];
-                    }
-                    else
-                    {
+                    } else {
                         $restr .= $pinyins[$c][0];
                     }
-                }else
-                {
+                } else {
                     $restr .= "_";
                 }
-            }else if( preg_match("/[a-z0-9]/i", $str[$i]) )
-            {
+            } else if (preg_match("/[a-z0-9]/i", $str[$i])) {
                 $restr .= $str[$i];
-            }
-            else
-            {
+            } else {
                 $restr .= "_";
             }
         }
-        if($isclose==0)
-        {
+        if ($isclose == 0) {
             unset($pinyins);
         }
     }
@@ -99,37 +85,29 @@ function SpGetPinyin($str, $ishead=0, $isclose=1)
  */
 function SpCreateDir($spath)
 {
-    global $cfg_dir_purview,$cfg_basedir,$cfg_ftp_mkdir,$isSafeMode;
-    if($spath=='')
-    {
+    global $cfg_dir_purview, $cfg_basedir, $cfg_ftp_mkdir, $isSafeMode;
+    if ($spath == '') {
         return true;
     }
     $flink = false;
     $truepath = $cfg_basedir;
-    $truepath = str_replace("\\","/",$truepath);
-    $spaths = explode("/",$spath);
+    $truepath = str_replace("\\", "/", $truepath);
+    $spaths = explode("/", $spath);
     $spath = "";
-    foreach($spaths as $spath)
-    {
-        if($spath=="")
-        {
+    foreach ($spaths as $spath) {
+        if ($spath == "") {
             continue;
         }
         $spath = trim($spath);
-        $truepath .= "/".$spath;
-        if(!is_dir($truepath) || !is_writeable($truepath))
-        {
-            if(!is_dir($truepath))
-            {
-                $isok = MkdirAll($truepath,$cfg_dir_purview);
+        $truepath .= "/" . $spath;
+        if (!is_dir($truepath) || !is_writeable($truepath)) {
+            if (!is_dir($truepath)) {
+                $isok = MkdirAll($truepath, $cfg_dir_purview);
+            } else {
+                $isok = ChmodAll($truepath, $cfg_dir_purview);
             }
-            else
-            {
-                $isok = ChmodAll($truepath,$cfg_dir_purview);
-            }
-            if(!$isok)
-            {
-                echo "创建或修改目录：".$truepath." 失败！<br>";
+            if (!$isok) {
+                echo "创建或修改目录：" . $truepath . " 失败！<br>";
                 CloseFtp();
                 return false;
             }
@@ -141,13 +119,13 @@ function SpCreateDir($spath)
 
 function jsScript($js)
 {
-	$out = "<script type=\"text/javascript\">";
-	$out .= "//<![CDATA[\n";
-	$out .= $js;
-	$out .= "\n//]]>";
-	$out .= "</script>\n";
+    $out = "<script type=\"text/javascript\">";
+    $out .= "//<![CDATA[\n";
+    $out .= $js;
+    $out .= "\n//]]>";
+    $out .= "</script>\n";
 
-	return $out;
+    return $out;
 }
 
 /**
@@ -162,45 +140,37 @@ function jsScript($js)
  * @param     string  $isfullpage 是否全屏
  * @return    string
  */
-function SpGetEditor($fname,$fvalue,$nheight="350",$etype="Basic",$gtype="print",$isfullpage="false",$bbcode=false)
+function SpGetEditor($fname, $fvalue, $nheight = "350", $etype = "Basic", $gtype = "print", $isfullpage = "false", $bbcode = false)
 {
     global $cfg_ckeditor_initialized;
-    if(!isset($GLOBALS['cfg_html_editor']))
-    {
-        $GLOBALS['cfg_html_editor']='fck';
+    if (!isset($GLOBALS['cfg_html_editor'])) {
+        $GLOBALS['cfg_html_editor'] = 'fck';
     }
-    if($gtype=="")
-    {
+    if ($gtype == "") {
         $gtype = "print";
     }
-    if($GLOBALS['cfg_html_editor']=='fck')
-    {
-        require_once(DEDEINC.'/FCKeditor/fckeditor.php');
+    if ($GLOBALS['cfg_html_editor'] == 'fck') {
+        require_once(DEDEINC . '/FCKeditor/fckeditor.php');
         $fck = new FCKeditor($fname);
-        $fck->BasePath        = $GLOBALS['cfg_cmspath'].'/include/FCKeditor/' ;
-        $fck->Width        = '100%' ;
-        $fck->Height        = $nheight ;
-        $fck->ToolbarSet    = $etype ;
+        $fck->BasePath        = $GLOBALS['cfg_cmspath'] . '/include/FCKeditor/';
+        $fck->Width        = '100%';
+        $fck->Height        = $nheight;
+        $fck->ToolbarSet    = $etype;
         $fck->Config['FullPage'] = $isfullpage;
-        if($GLOBALS['cfg_fck_xhtml']=='Y')
-        {
+        if ($GLOBALS['cfg_fck_xhtml'] == 'Y') {
             $fck->Config['EnableXHTML'] = 'true';
             $fck->Config['EnableSourceXHTML'] = 'true';
         }
-        $fck->Value = $fvalue ;
-        if($gtype=="print")
-        {
+        $fck->Value = $fvalue;
+        if ($gtype == "print") {
             $fck->Create();
-        }
-        else
-        {
+        } else {
             return $fck->CreateHtml();
         }
-    }
-    else if($GLOBALS['cfg_html_editor']=='ckeditor')
-    {
+    } else if ($GLOBALS['cfg_html_editor'] == 'ckeditor') {
         $addConfig = "";
         if (defined("DEDEADMIN")) {
+            // $addConfig = ",{filebrowserImageUploadUrl:'./dialog/select_images_post.php',filebrowserUploadUrl:'./dialog/select_media_post.php'}";
             $addConfig = ",{filebrowserImageUploadUrl:'./dialog/select_images_post.php'}";
         }
         $code = <<<EOT
@@ -210,15 +180,12 @@ function SpGetEditor($fname,$fvalue,$nheight="350",$etype="Basic",$gtype="print"
 var editor = CKEDITOR.replace('{$fname}'{$addConfig});
 </script>
 EOT;
-        if($gtype=="print")
-        {
+        if ($gtype == "print") {
             echo $code;
-        }
-        else
-        {
+        } else {
             return $code;
         }
-    } else { 
+    } else {
         /*
         // ------------------------------------------------------------------------
         // 当前版本,暂时取消dedehtml编辑器的支持
@@ -249,12 +216,11 @@ EOT;
  */
 function SpGetNewInfo()
 {
-    global $cfg_version_detail,$dsql;
+    global $cfg_version_detail, $dsql;
     $nurl = $_SERVER['HTTP_HOST'];
-    if( preg_match("#[a-z\-]{1,}\.[a-z]{2,}#i",$nurl) ) {
+    if (preg_match("#[a-z\-]{1,}\.[a-z]{2,}#i", $nurl)) {
         $nurl = urlencode($nurl);
-    }
-    else {
+    } else {
         $nurl = "test";
     }
     $phpv = phpversion();
@@ -263,13 +229,11 @@ function SpGetNewInfo()
     $add_query = '';
     $query = "SELECT COUNT(*) AS dd FROM `#@__member` ";
     $row1 = $dsql->GetOne($query);
-    if ( $row1 ) $add_query .= "&mcount={$row1['dd']}";
+    if ($row1) $add_query .= "&mcount={$row1['dd']}";
     $query = "SELECT COUNT(*) AS dd FROM `#@__arctiny` ";
     $row2 = $dsql->GetOne($query);
-    if ( $row2 ) $add_query .= "&acount={$row2['dd']}";
-    
-    $offUrl = DEDEBIZURL."/version?version={$cfg_version_detail}&formurl={$nurl}&phpver={$phpv}&os={$sp_os}&mysqlver={$mysql_ver}{$add_query}";
+    if ($row2) $add_query .= "&acount={$row2['dd']}";
+
+    $offUrl = DEDEBIZURL . "/version?version={$cfg_version_detail}&formurl={$nurl}&phpver={$phpv}&os={$sp_os}&mysqlver={$mysql_ver}{$add_query}";
     return $offUrl;
 }
-
-?>
--- src/static/ckeditor/config.js
+++ src/static/ckeditor/config.js
@@ -3,24 +3,24 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
+CKEDITOR.editorConfig = function (config) {
 	config.toolbarGroups = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'document', groups: ['mode', 'document', 'doctools'] },
+		{ name: 'clipboard', groups: ['clipboard', 'undo'] },
+		{ name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
+		{ name: 'forms', groups: ['forms'] },
 		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+		{ name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'] },
+		{ name: 'links', groups: ['links'] },
+		{ name: 'insert', groups: ['insert'] },
 		'/',
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'tools', groups: [ 'tools' ] }
+		{ name: 'styles', groups: ['styles'] },
+		{ name: 'colors', groups: ['colors'] },
+		{ name: 'tools', groups: ['tools'] }
 	];
 
-	config.extraPlugins = 'dedepagebreak';
+	config.extraPlugins = 'html5video,dedepagebreak';
 
 	config.removeButtons = 'About,ShowBlocks,Iframe,Flash,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField';
 
