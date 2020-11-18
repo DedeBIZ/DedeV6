00f596d16bd405ca6c9e033210155bc51d8ba71e
tianya <tianya@imcollege.cn>
2020-11-18 22:20:36 +0800

    调整默认水印

--- src/data/mark/inc_photowatermark_config.php
+++ src/data/mark/inc_photowatermark_config.php
@@ -5,10 +5,9 @@ $photo_marktype = '1';
 $photo_wwidth = '120';
 $photo_wheight = '120';
 $photo_waterpos = '9';
-$photo_watertext = 'www.dedecms.com';
+$photo_watertext = 'www.dedebiz.com';
 $photo_fontsize = '20';
 $photo_fontcolor = '0,0,0';
 $photo_marktrans = '100';
 $photo_diaphaneity = '100';
 $photo_markimg = 'mark.png';
-?>
