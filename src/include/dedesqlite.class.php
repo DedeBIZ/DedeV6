c11c933de260b7f44dc0065b360ed68e88025681
llgoer <llgoer@github.com>
2021-01-01 18:45:35 +0800

    命令行工具增加一个用于本地调试的快速开始环境

--- src/include/dedesqlite.class.php
+++ src/include/dedesqlite.class.php
@@ -513,6 +513,7 @@ class DedeSqlite
         //var_dump($this->queryString);
         $this->queryString = preg_replace("/Show Tables/i", "SELECT name FROM sqlite_master WHERE type = \"table\"", $this->queryString);
         $this->queryString = str_replace("\'", "\"", $this->queryString);
+        $this->queryString = str_replace('\t\n', "", $this->queryString);
         //var_dump($this->queryString);
     }
 
