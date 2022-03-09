84619c296a347d02c018486621abc63c7a960889
xushubieli <xushubieli@qq.com>
2022-03-09 11:55:26 +0800

    修改文档列表和专题发布

--- src/admin/inc/inc_menu.php
+++ src/admin/inc/inc_menu.php
@@ -75,7 +75,7 @@ if ($cuserLogin->getUserType() >= 10) {
 }
 $menusMain = "<m:top item='1_' name='常用操作' display='block'>
     <m:item name='网站栏目管理' link='catalog_main.php' ischannel='1' addalt='创建栏目' linkadd='catalog_add.php?listtype=all' rank='t_List,t_AccList' target='main' />
-    <m:item name='所有档案列表' link='content_list.php' rank='a_List,a_AccList' target='main' />
+    <m:item name='所有文档列表' link='content_list.php' rank='a_List,a_AccList' target='main' />
     <m:item name='等审核的档案' link='content_list.php?arcrank=-1' rank='a_Check,a_AccCheck' target='main' />
     <m:item name='我发布的文档' link='content_list.php?mid=".$cuserLogin->getUserID()."' rank='a_List,a_AccList,a_MyList' target='main' />
     <m:item name='评论管理' link='feedback_main.php' rank='sys_Feedback' target='main' />
