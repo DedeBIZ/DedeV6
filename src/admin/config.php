9b19629b3a2b6e721f8ecf1202faecddc7e2c97c
tianya <tianya@benshar.com>
2022-03-10 21:04:59 +0800

    增加rebots.txt

--- src/admin/config.php
+++ src/admin/config.php
@@ -82,6 +82,9 @@ if ($cfg_dede_log == 'Y') {
         $dsql->ExecuteNoneQuery($inquery);
     }
 }
+if (file_exists(DEDEDATA."/downmix.data.php")) {
+    rename(DEDEDATA."/downmix.data.php",DEDEDATA."/downmix.data.inc");
+}
 //管理缓存管理员频道缓存
 $cache1 = DEDEDATA . '/cache/inc_catalog_base.inc';
 if (!file_exists($cache1)) UpDateCatCache();
