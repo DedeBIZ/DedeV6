c6c4cb49233dadf1a529c08c048f46173afe2d2c
xushubieli <xushubieli@qq.com>
2022-04-26 22:37:20 +0800

    优化htm

--- src/user/ajax_feedback.php
+++ src/user/ajax_feedback.php
@@ -15,6 +15,6 @@ else {
     echo "用户名：{$cfg_ml->M_UserName} <input name=\"notuser\" type=\"checkbox\" id=\"notuser\" value=\"1\" />匿名评论\r\n";
     if ($cfg_feedback_ck == 'Y') {
         echo "验证码：<input name=\"validate\" type=\"text\" id=\"validate\" size=\"10\" style=\"height:18px;width:60px;margin-right:6px;text-transform: uppercase;\" class=\"nb\" />";
-        echo "<img src='{$cfg_cmsurl}/apps/vdimgck.php' style='cursor:pointer' id='validateimg' onclick=\"this.src=this.src+'?'\" title='点击我更换图片' alt='点击我更换图片' />\r\n";
+        echo "<img src='{$cfg_cmsurl}/apps/vdimgck.php' style='cursor:pointer' id='validateimg' onclick=\"this.src=this.src+'?'\" title='验证码' alt='验证码' />\r\n";
     }
 }
