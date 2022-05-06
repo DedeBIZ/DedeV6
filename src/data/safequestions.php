cfb6a54b1a9670a983fce6a7924184e69cb92093
xushubieli <xushubieli@qq.com>
2022-05-06 14:30:29 +0800

    常规修改

--- src/data/safequestions.php
+++ src/data/safequestions.php
@@ -2,10 +2,7 @@
 //安全提示问题，这两句不要修改
 $safequestions = array();
 $safequestions[0] = '没安全提示问题';
-
 //下面的设置可以手工修改
-//start****************************
-
 $safequestions[1] = '您最喜欢的格言什么';
 $safequestions[2] = '您家乡的名称是什么';
 $safequestions[3] = '您读的小学叫什么';
@@ -13,9 +10,6 @@ $safequestions[4] = '您的父亲叫什么名字';
 $safequestions[5] = '您的母亲叫什么名字';
 $safequestions[6] = '您最喜欢的偶像是谁';
 $safequestions[7] = '您最喜欢的歌曲是什么';
-
-//end****************************
-
 //以下不要修改
 function GetSafequestion($selid=0,$formname='safequestion')
 {
@@ -29,5 +23,4 @@ function GetSafequestion($selid=0,$formname='safequestion')
 	$safequestions_form .= "</select>\r\n";
 	return $safequestions_form;
 }
-
 ?>
