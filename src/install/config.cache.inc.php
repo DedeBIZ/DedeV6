1a5de492e656495b88995f7b37ffa9a8c11877e2
xushubieli <xushubieli@qq.com>
2022-05-21 22:04:39 +0800

    Update config.cache.inc.php

--- src/install/config.cache.inc.php
+++ src/install/config.cache.inc.php
@@ -5,6 +5,7 @@ $cfg_cookie_encode = '~cookieEncode~';
 $cfg_indexurl = '~indexurl~';
 $cfg_backup_dir = 'backupdata';
 $cfg_indexname = '网站主页';
+$cfg_indexseo = '~indexseo~';
 $cfg_webname = '~webname~';
 $cfg_adminemail = '~adminmail~';
 $cfg_html_editor = 'ckeditor';
