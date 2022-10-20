4753fd1e7d9a16b09686488585304d77dcfe36ac
xushubieli <xushubieli@qq.com>
2022-10-20 11:51:14 +0800

    Update index_testenv.php

--- src/admin/index_testenv.php
+++ src/admin/index_testenv.php
@@ -154,7 +154,7 @@ if ($rs < 0) {
 <?php
 if (count($safeMsg) > 0) {
 ?>
-	<div class="alert alert-danger mt-3 mb-3">
+	<div class="alert alert-danger mt-3">
 		<?php
 		$i = 1;
 		foreach ($safeMsg as $key => $val) {
