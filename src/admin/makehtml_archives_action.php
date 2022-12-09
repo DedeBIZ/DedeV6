32be59053e1c1e541539b95e814c6513c51f244e
叙述、别离 <93301500+xushubieli@users.noreply.github.com>
2022-12-09 10:55:15 +0800

    优化更新进度条

--- src/admin/makehtml_archives_action.php
+++ src/admin/makehtml_archives_action.php
@@ -79,8 +79,8 @@ $ttime = time() - $sstime;
 $ttime = number_format(($ttime / 60), 2);
 //返回提示信息
 $tjlen = $totalnum > 0 ? ceil(($tjnum / $totalnum) * 100) : 100;
-$tjsta = "<div style='width:260px;height:16px;text-align:left;border:1px solid #1eb867;border-radius:.2rem'><div style='max-width:260px;width:$tjlen%;height:16px;background:#1eb867'></div></div>";
-$tjsta .= "<br>到达位置<span class='text-primary'>".($startdd + $pagesize)."</span>，用时<span class='text-primary'>{$ttime}</span>分钟<br>完成更新文档总数<span class='text-primary'>$tjlen</span>%";
+$tjsta = "<div style='margin-bottom:18px;margin-bottom:18px;width:260px;height:18px;text-align:left;border:1px solid #1eb867;border-radius:.2rem'><div style='max-width:260px;width:$tjlen%;height:18px;background:#1eb867'></div></div>";
+$tjsta .= "到达位置<span class='text-primary'>".($startdd + $pagesize)."</span>，用时<span class='text-primary'>{$ttime}</span>分钟，完成更新文档总数<span class='text-primary'>$tjlen</span>%";
 //速度测试
 if ($tjnum < $totalnum) {
     $nurl  = "makehtml_archives_action.php?endid=$endid&startid=$startid&typeid=$typeid";
