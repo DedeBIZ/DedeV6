9dbed583d11429cedd3f63530c82a5dade2b711a
tianya <yanghuxiao@vip.qq.com>
2023-03-02 00:03:01 +0800

    bump6.2.5

--- src/system/common.inc.php
+++ src/system/common.inc.php
@@ -186,7 +186,7 @@ $cfg_soft_dir = $cfg_medias_dir.'/soft';
 $cfg_other_medias = $cfg_medias_dir.'/media';
 //软件摘要信息，请不要删除，否则系统无法正确接收系统漏洞或升级信息
 $cfg_version = 'V6';
-$cfg_version_detail = '6.2.5beta2';//详细版本号
+$cfg_version_detail = '6.2.5';//详细版本号
 $cfg_soft_lang = 'utf-8';
 $cfg_soft_public = 'base';
 $cfg_softname = '得德系统';
