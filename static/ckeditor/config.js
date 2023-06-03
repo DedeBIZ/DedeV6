55854c868a076f13e9f0579f1258845153f03afa
叙述、别离 <vaecq@zhelixie.com>
2023-06-03 17:40:19 +0800

    上传文件至 'static/ckeditor'

---
+++ static/ckeditor/config.js
@@ -0,0 +1,23 @@
+﻿/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+CKEDITOR.editorConfig = function (config) {
+	config.toolbarGroups = [
+		{ name: 'document', groups: ['mode', 'document', 'doctools'] },
+		{ name: 'clipboard', groups: ['undo', 'cleanup', 'clipboard'] },
+		{ name: 'styles', groups: ['styles'] },
+		{ name: 'colors', groups: ['colors'] },
+		{ name: 'paragraph', groups: ['align', 'paragraph', 'textindent', 'indent'] },
+		{ name: 'basicstyles', groups: ['basicstyles', 'list', 'bidi', 'blocks'] },
+		{ name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
+		{ name: 'links', groups: ['links'] },
+		{ name: 'insert', groups: ['insert'] },
+		{ name: 'tools', groups: ['tools'] }
+	];
+	config.height = 360;
+	config.removePlugins = 'exportpdf';
+	config.extraPlugins = 'html5video,dedepagebreak,textindent';
+	config.removeButtons = 'Save,NewPage,Print,Preview,Templates,Smiley,About,ShowBlocks,Iframe,Flash,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField';
+	config.font_names='宋体/SimSun;楷体/KaiTi;黑体/SimHei;微软雅黑/Microsoft YaHei;微软正黑/Microsoft JhengHei'+ config.font_names;
+};
