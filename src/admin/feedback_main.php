0a38d4fee13f9427b0e4231e808efa8ca338fb3e
叙述、别离 <2449271624@qq.com>
2023-10-16 10:13:46 +0800

    移除审核按钮调整

--- src/admin/feedback_main.php
+++ src/admin/feedback_main.php
@@ -16,7 +16,7 @@ require_once(DEDEINC."/typelink/typelink.class.php");
 setcookie("ENV_GOBACK_URL", $dedeNowurl, time() + 3600, "/");
 function IsCheck($st)
 {
-    return $st == 1 ? '<span class="btn btn-success btn-sm">已审核</span>' : '<span class="btn btn-warning btn-sm">未审核</span>';
+    return $st == 1 ? '已审核':'未审核';
 }
 function jsTrimjajx($str, $len)
 {
