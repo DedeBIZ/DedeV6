fc53e7c4f8131e2f8eebd84345d3fa3c931dede6
叙述、别离 <2449271624@qq.com>
2023-12-20 15:42:20 +0800

    Update hotwords.lib.php

--- src/system/taglib/hotwords.lib.php
+++ src/system/taglib/hotwords.lib.php
@@ -25,7 +25,7 @@ function lib_hotwords(&$ctag, &$refObj)
     $dsql->Execute('hw');
     $hotword = '';
     while ($row = $dsql->GetArray('hw')) {
-        $hotword .= " <a href='".$cfg_phpurl."/search.php?keyword=".urlencode($row['keyword'])."'>".$row['keyword']."</a>";
+        $hotword .= "<a href='".$cfg_phpurl."/search.php?keyword=".urlencode($row['keyword'])."'>".$row['keyword']."</a> ";
     }
     return $hotword;
 }
